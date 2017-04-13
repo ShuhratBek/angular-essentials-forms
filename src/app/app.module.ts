3a0d00e4c21593718646580f81e1e5391cab9bda
Shukhratbek_Mamadaliev <Shukhratbek_Mamadaliev@epam.com>
2017-04-14 00:40:53 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule, JsonpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import { HeroFormComponent } from './hero';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeroFormComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        JsonpModule,
+        // routing
+    ],
+    // providers: [appRoutingProviders],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
